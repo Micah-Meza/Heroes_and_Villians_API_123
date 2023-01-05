26591078462e2421d8af461ec7a97832e1b91ab2
Micah <airrenno@yahoo.com>
2023-01-04 21:15:38 -0600

    Updates Super_types serializer, urls, admin files + Created database + created model type + Added app to settings list.

--- project_heroes/super_types/models.py
+++ project_heroes/super_types/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class SuperType(models.Model):
+    hero = 'Hero'
+    villian = 'Villian'
+    super_choices = [ (hero, 'Hero'),
+                    (villian, 'Villian'),
+    ]
+
+    type = models.CharField(max_length = 7, choices = super_choices, default = "")
+
+
+
